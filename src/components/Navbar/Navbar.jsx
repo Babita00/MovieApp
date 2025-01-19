2c0be5a5074d3a0bae2b4a57f6c4db311e27152d
Babita00 <babitaasth07@gmail.com>
2025-01-19 16:29:31 +0545

    refactor Navbar component to use default export and update styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
 
-export const Navbar = () => {
+ const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className=" p-4 h-15 w-full bg-black">
+        <div>
+            <img src="reactMovie_logo.png" className="p-3 h-12"/>
+        </div>
+    </div>
   )
 }
+
+export default Navbar
