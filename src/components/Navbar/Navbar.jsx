289ff7bdc65b958c8b4379ece4ef8fbb93767580
Babita00 <babitaasth07@gmail.com>
2025-01-20 16:21:17 +0545

    add navigation menu to Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 
  const Navbar = () => {
   return (
-    <div className=" p-4 h-15 w-full bg-black">
+    <div className=" p-4 h-15 w-full bg-black flex  ">
         <div>
             <img src="reactMovie_logo.png" className="p-3 h-12"/>
+        </div>
+
+        <div className="navbar-menu p-4 ">
+          <ul className="flex space-x-6 text-white cursor-pointer">
+            <li><a href="/"className="hover:text-grey-500 "/>Home</li>
+            <li><a href="/"className="hover:text-grey-500"/>About</li>
+          </ul>
         </div>
     </div>
   )
