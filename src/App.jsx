2fce06a71bbf26aa3ec5f3e35abf6391fb468334
Babita00 <babitaasth07@gmail.com>
2025-01-20 16:45:15 +0545

    add routing functionality with react-router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import "./App.css";
 import MoviesPage from "./components/Movies/MoviePage";
 import Navbar from "./components/Navbar/Navbar";
+import { createRoot } from "react-dom/client";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
-  
+  const myRoute = createBrowserRouter([
+    {
+      path: "/",
+      element: <h1>Home</h1>,
+    },
+    { path: "/about", element: <h1>About</h1> },
+  ]);
+
   return (
     <>
-     <Navbar/>
-     <MoviesPage/>
+      <Navbar />
+      <MoviesPage />
+
+      <RouterProvider router={myRoute} />
     </>
   );
 }
