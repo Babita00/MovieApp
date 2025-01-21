0394a528c786362d9e9ac73860db41b3f78d826b
Babita00 <babitaasth07@gmail.com>
2025-01-21 16:41:03 +0545

    remove Navbar component from routing structure in App for simplification

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,28 @@ import "./App.css";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Home } from "./components/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
+// import Navbar from "./components/Navbar/Navbar";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  
+} from "react-router-dom";
 import { PageNotFound } from "./components/PageNotFound/PageNotFound";
 
 function App() {
   const myRoute = createBrowserRouter([
-    {
-      path: "/",
-      element: <Navbar />,
-      children: [
-        { path: "/home", element: <Home /> },
-        { path: "/about", element: <About /> },
-        { path: "/contact", element: <Contact /> },
-        { path: "*", element: <PageNotFound /> },
-      ],
-    },
+    { path: "/", element:<Home/> },
+    { path: "/about", element: <About/> },
+    { path: "/contact", element:<Contact/> },
+    { path: "*", element:<PageNotFound/> },
   ]);
 
   return (
     <>
+      {/* <Navbar /> */}
+      {/* <MoviesPage /> */}
+
       <RouterProvider router={myRoute} />
     </>
   );
