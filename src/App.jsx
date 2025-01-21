4cc874f25226056d31d1d6989e307b575626c7b8
Babita00 <babitaasth07@gmail.com>
2025-01-21 16:59:46 +0545

    add MovieDetails route to App for detailed movie view

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Home } from "./components/Home/Home";
+import { MovieDetails } from "./components/MovieDetails/MovieDetails";
 // import Navbar from "./components/Navbar/Navbar";
 
 import {
@@ -16,6 +17,7 @@ function App() {
     { path: "/", element:<Home/> },
     { path: "/about", element: <About/> },
     { path: "/contact", element:<Contact/> },
+    { path: "/movie-details/:id", element:<MovieDetails/> },
     { path: "*", element:<PageNotFound/> },
   ]);
 
