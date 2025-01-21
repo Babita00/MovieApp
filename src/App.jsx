b24cf9131a0d86de59e82643550299404992bae2
Babita00 <babitaasth07@gmail.com>
2025-01-21 15:57:50 +0545

    add PageNotFound component for handling undefined routes in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,22 @@ import "./App.css";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Home } from "./components/Home/Home";
-import MoviesPage from "./components/Movies/MoviePage";
 import Navbar from "./components/Navbar/Navbar";
 import { createRoot } from "react-dom/client";
 
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
+  
 } from "react-router-dom";
+import { PageNotFound } from "./components/PageNotFound/PageNotFound";
 
 function App() {
   const myRoute = createBrowserRouter([
     { path: "/", element:<Home/> },
     { path: "/about", element: <About/> },
     { path: "/contact", element:<Contact/> },
-    { path: "/movie", element:<MoviesPage/> },
+    { path: "*", element:<PageNotFound/> },
   ]);
 
   return (
