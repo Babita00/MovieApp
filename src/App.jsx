bffaf799d68d88f634ac991e3d398eb812910f23
Babita00 <babitaasth07@gmail.com>
2025-01-21 16:24:39 +0545

    refactor routing structure in App component to include Navbar and nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,25 @@ import { Contact } from "./components/Contact/Contact";
 import { Home } from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { PageNotFound } from "./components/PageNotFound/PageNotFound";
 
 function App() {
   const myRoute = createBrowserRouter([
-    { path: "/", element:<Home/> },
-    { path: "/about", element: <About/> },
-    { path: "/contact", element:<Contact/> },
-    { path: "*", element:<PageNotFound/> },
+    {
+      path: "/",
+      element: <Navbar />,
+      children: [
+        { path: "/home", element: <Home /> },
+        { path: "/about", element: <About /> },
+        { path: "/contact", element: <Contact /> },
+        { path: "*", element: <PageNotFound /> },
+      ],
+    },
   ]);
 
   return (
     <>
-      <Navbar />
-      {/* <MoviesPage /> */}
-
       <RouterProvider router={myRoute} />
     </>
   );
