dbd23c03f8097b25651a5cd771ac5736e6bb74b0
Babita00 <babitaasth07@gmail.com>
2025-01-21 16:41:29 +0545

    refactor Navbar component to use NavLink for routing and add click handler for logo navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,43 @@
-import { Link } from "react-router-dom"
- const Navbar = () => {
+// import { Link } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom";
+const Navbar = () => {
+  const navigate = useNavigate();
+
+  const onImageClick = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
   return (
     <div className=" p-4 h-15 w-full bg-black flex  ">
-        <div>
-            <img src="reactMovie_logo.png" className="p-3 h-12"/>
-        </div>
+      <div>
+        <img
+          src="reactMovie_logo.png"
+          className="p-3 h-12"
+          onClick={onImageClick}
+        />
+      </div>
 
-        <div className="navbar-menu p-4 ">
-          <ul className="flex space-x-6 text-white cursor-pointer">
-            <li><Link to="/home"className="hover:text-grey-500 ">Home</Link></li>
-            <li><Link to="/about"className="hover:text-grey-500">About</Link></li>
-            <li><Link to="/contact"className="hover:text-grey-500">Contact</Link></li>
-          </ul>
-        </div>
+      <div className="navbar-menu p-4 ">
+        <ul className="flex space-x-6 text-white cursor-pointer">
+          <li>
+            <NavLink to="/" className="hover:text-grey-500">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className="hover:text-grey-500">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className="hover:text-grey-500">
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
