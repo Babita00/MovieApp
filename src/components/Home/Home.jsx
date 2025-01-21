a45ac73ae88195af6cdff1b99a9685a9c1dfcf7c
Babita00 <babitaasth07@gmail.com>
2025-01-21 16:22:43 +0545

    add Navbar component to Home for improved layout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,11 @@
 import MoviesPage from "../Movies/MoviePage";
+import Navbar from "../Navbar/Navbar";  
 export const Home = () => {
   return (
+    <>
+    <Navbar />
+
    <MoviesPage/>
+   </>
   )
 }
