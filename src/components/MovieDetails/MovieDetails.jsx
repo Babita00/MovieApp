59339c45e652dd482da06676b16d7e8021c78a03
Babita00 <babitaasth07@gmail.com>
2025-01-23 21:04:27 +0545

    refactor MovieDetails component layout for improved styling and structure

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -35,27 +35,34 @@ export const MovieDetails = () => {
   }
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-gray-900 text-white rounded-lg shadow-lg">
-      <h1 className="text-3xl font-bold mb-4 text-center">
-        {movieDetails.title}
-      </h1>
-      <img
-        className="w-full md:w-1/2 mx-auto rounded-lg shadow-md"
-        src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
-        alt={movieDetails.title}
-      />
-      <p className="mt-4 text-gray-300">
-        <strong className="text-white">Description:</strong>{" "}
-        {movieDetails.overview}
-      </p>
-      <p className="mt-2 text-gray-300">
-        <strong className="text-white">Rating:</strong>{" "}
-        {movieDetails.vote_average} / 10
-      </p>
+    <div className=" mx-auto p-6 bg-gray-900 text-white rounded-lg shadow-lg">
+      <div className="flex bg-gray-700">
+        <img
+          className="w-full md:w-1/2 mx-auto rounded-lg shadow-md"
+          src={`https://image.tmdb.org/t/p/w500${movieDetails.poster_path}`}
+          alt={movieDetails.title}
+        />
+
+        <div className="flex flex-col">
+          <h1 className="text-3xl font-bold mb-4 text-center">
+            {movieDetails.title}
+          </h1>
+
+          <p className="mt-4 text-gray-300 flex flex-col mx-10">  
+            <strong className="text-white">PLOT:</strong>{" "}
+            {movieDetails.overview}
+          </p>
+
+          <p className="mt-2 text-gray-300 mx-10">
+            <strong className="text-white">Rating:</strong>{" "}
+            {movieDetails.vote_average} / 10
+          </p>
+        </div>
+      </div>
 
       <h2 className="text-2xl font-semibold mt-6 mb-4">Actors</h2>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {movieDetails.credits?.cast.slice(0, 5).map((actor) => (
+        {movieDetails.credits?.cast.slice(0).map((actor) => (
           <li
             key={actor.id}
             className="flex flex-col items-center bg-gray-800 rounded-lg p-4 shadow-md"
