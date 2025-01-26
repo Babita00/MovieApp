9522561e39aba8e044f60ab0fdbffcfbcba5a9f9
Babita00 <babitaasth07@gmail.com>
2025-01-26 14:33:31 +0545

    refactor routing in App component to use createHashRouter for better compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,16 @@ import { Home } from "./components/Home/Home";
 import { MovieDetails } from "./components/MovieDetails/MovieDetails";
 // import Navbar from "./components/Navbar/Navbar";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  
-} from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import { PageNotFound } from "./components/PageNotFound/PageNotFound";
 
 function App() {
-  const myRoute = createBrowserRouter([
-    { path: "/", element:<Home/> },
-    { path: "/about", element: <About/> },
-    { path: "/contact", element:<Contact/> },
-    { path: "/movie-details/:id?", element:<MovieDetails/> },
-    { path: "*", element:<PageNotFound/> },
+  const myRoute = createHashRouter([
+    { path: "/", element: <Home /> },
+    { path: "/about", element: <About /> },
+    { path: "/contact", element: <Contact /> },
+    { path: "/movie-details/:id?", element: <MovieDetails /> },
+    { path: "*", element: <PageNotFound /> },
   ]);
 
   return (
