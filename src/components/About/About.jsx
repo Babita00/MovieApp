96f0faa6f78aeace43f923ccc558e84b1496b91c
Babita00 <babitaasth07@gmail.com>
2025-03-17 13:40:48 +0545

    refactor: remove Navbar component from About page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,6 @@
-import Navbar from "../Navbar/Navbar";
-
 export const About = () => {
   return (
     <>
-      <Navbar />
       <div>
         <h1 className="text-black">About</h1>
       </div>
