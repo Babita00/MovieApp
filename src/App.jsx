62a4f6f19aac125500d6925b9de58e567def23a8
Babita00 <babitaasth07@gmail.com>
2025-03-17 13:40:41 +0545

    refactor: implement RootLayout component with Navbar and nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,39 @@ import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Home } from "./components/Home/Home";
 import { MovieDetails } from "./components/MovieDetails/MovieDetails";
-// import Navbar from "./components/Navbar/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider, Outlet } from "react-router-dom";
 import { PageNotFound } from "./components/PageNotFound/PageNotFound";
+
+// Create a root layout component that includes the navbar
+function RootLayout() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
 
 function App() {
   const myRoute = createHashRouter([
-    { path: "/", element: <Home /> },
-    { path: "/about", element: <About /> },
-    { path: "/contact", element: <Contact /> },
-    { path: "/movie-details/:id?", element: <MovieDetails /> },
-    { path: "*", element: <PageNotFound /> },
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "about", element: <About /> },
+        { path: "contact", element: <Contact /> },
+        { path: "movie-details/:id?", element: <MovieDetails /> },
+        { path: "*", element: <PageNotFound /> },
+      ],
+    },
   ]);
 
-  return (
-    <>
-      {/* <Navbar /> */}
-      {/* <MoviesPage /> */}
-
-      <RouterProvider router={myRoute} />
-    </>
-  );
+  return <RouterProvider router={myRoute} />;
 }
 
 export default App;
