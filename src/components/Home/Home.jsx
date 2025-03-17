45162b21c06e72f94a5e33c840c4a895ec2689d0
Babita00 <babitaasth07@gmail.com>
2025-03-17 13:41:12 +0545

    refactor: remove Navbar component from Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,8 @@
 import MoviesPage from "../Movies/MoviePage";
-import Navbar from "../Navbar/Navbar";  
 export const Home = () => {
   return (
     <>
-    <Navbar />
-
-   <MoviesPage/>
-   </>
-  )
-}
+      <MoviesPage />
+    </>
+  );
+};
